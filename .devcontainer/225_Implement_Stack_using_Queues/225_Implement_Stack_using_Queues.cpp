02fb99231f2793de6a16f690d224604f25c235cc
Juan Echavarria <juanea@gmail.com>
2023-02-11 18:15:50 -0800

    933 Number Of Recent Call

---
+++ .devcontainer/225_Implement_Stack_using_Queues/225_Implement_Stack_using_Queues.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+
+class MyStack
+{
+public:
+  MyStack()
+  {
+    std::queue<int> myStackWithQueue;
+  }
+
+  void push(int x)
+  {
+    myStackWithQueue.
+  }
+
+  int pop()
+  {
+  }
+
+  int top()
+  {
+  }
+
+  bool empty()
+  {
+  }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+int main()
+{
+  MyStack *obj = new MyStack;
+  obj->push(x);
+  int param_2 = obj->pop();
+  int param_3 = obj->top();
+  bool param_4 = obj->empty();
+  return 0;
+}
---
+++ 933_Number_of_Recent_Calls/933_Number_of_Recent_Calls.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+
+class RecentCounter
+{
+public:
+  RecentCounter()
+  {
+  }
+
+  int ping(int t)
+  {
+  }
+};
+
+int main()
+{
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
